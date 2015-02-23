02a230d134c58495640a5ad2584cd1f82fa3b16b
sean niesen <seanniesen@gmail.com>
2015-02-23 13:50:18 -0800

    get google api working with sunrise api

--- app.js
+++ app.js
@@ -10,35 +10,31 @@ app.controller('MainCtrl', [
 			var address = $scope.address + ", " + $scope.city + ", " + $scope.state;
 			address = address.toString();
 
-			$.ajax({
-    		url: "http://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&callback=mycallback",
+			var geocoder = new google.maps.Geocoder();
+			geocoder.geocode( { 'address': address}, function(results, status) {
+  			if (status == google.maps.GeocoderStatus.OK)
+  		{
+
+   		//make api call inside of call to google maps api
+   		var sunriseurl = "http://api.sunrise-sunset.org/json?lat=" + results[0].geometry.location.lat() + "&lng=" + results[0].geometry.location.lng() + "&callback=mycallback"
+   		$.ajax({
+    		url: sunriseurl,
     		dataType: "JSONP",
     		success: function(data) {
-    			var json = data;
-    			console.log(json.results.sunrise);
+    			$("#sunrise").text(data.results.sunrise);
+    			console.log(data);
+    			console.log(sunriseurl);
     		}
 			});
 
+  			}
+			});
 
-			// var geocoder = new google.maps.Geocoder();
-			// geocoder.geocode( { 'address': address}, function(results, status) {
-  	// 		if (status == google.maps.GeocoderStatus.OK)
-  	// 	{
-   //    // do something with the geocoded result
-   //    //
+			$scope.address = '';
+			$scope.city = '';
+			$scope.state = '';
+			$scope.startDate = '';
+			$scope.endDate = '';
 
-   //    console.log(results);
-			// console.log(results[0].geometry.location.lat());
-   //    console.log(results[0].geometry.location.lat());
-  	// 		}
-			// });
-
-			// $scope.address = '';
-			// $scope.city = '';
-			// $scope.state = '';
-			// $scope.startDate = '';
-			// $scope.endDate = '';
 		}; //end of getTimes();
-									 
-
 	}]);
