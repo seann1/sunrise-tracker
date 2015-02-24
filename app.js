3578d3b589a17c922d497d182356a405e4af69d6
sean niesen <seanniesen@gmail.com>
2015-02-23 20:29:08 -0800

    working on while loop for dates

--- app.js
+++ app.js
@@ -12,26 +12,27 @@ app.controller('MainCtrl', [
 
 			var startDate = $scope.startDate;
 			var startDateMilli = new Date($scope.startDate).getTime();
+			var endDateMilli = new Date($scope.endDate).getTime();
+
+
 
 			var geocoder = new google.maps.Geocoder();
 			geocoder.geocode( { 'address': address}, function(results, status) {
   			if (status == google.maps.GeocoderStatus.OK)
-  		{
-
-  			console.log(startDateMilli);
+  			{
 
    		//make api call inside of call to google maps api
-   		var sunriseurl = "http://api.sunrise-sunset.org/json?lat=" + results[0].geometry.location.lat() + "&lng=" + results[0].geometry.location.lng() + "&date=" + startDate + "&callback=mycallback"
-   		$.ajax({
-    		url: sunriseurl,
-    		dataType: "JSONP",
-    		success: function(data) {
-    			$("#sunrise").text(data.results.sunrise);
-    			console.log(data);
-    			console.log(sunriseurl);
-    		}
-			});
-
+	   			while (startDateMilli <= endDateMilli) {
+	 					var sunriseurl = "http://api.sunrise-sunset.org/json?lat=" + results[0].geometry.location.lat() + "&lng=" + results[0].geometry.location.lng() + "&date=" + startDate + "&callback=mycallback"
+			   		$.ajax({
+			    		url: sunriseurl,
+			    		dataType: "JSONP",
+			    		success: function(data) {
+			    			$("#sunrise").text(data.results.sunrise);
+			    			console.log(data);
+			    		} 	//end of success function
+						}); //end of api call
+			   	} //end of while loop
   			}
 			});
 
