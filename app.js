efa7e31826d31da4f0de4cd1f03f9260003728c5
sean niesen <seanniesen@gmail.com>
2015-02-24 15:16:26 -0800

    restructuring api call

--- app.js
+++ app.js
@@ -23,15 +23,18 @@ app.controller('MainCtrl', [
 
    		//make api call inside of call to google maps api
 	   			while (startDateMilli <= endDateMilli) {
-	 					var sunriseurl = "http://api.sunrise-sunset.org/json?lat=" + results[0].geometry.location.lat() + "&lng=" + results[0].geometry.location.lng() + "&date=" + startDate + "&callback=mycallback"
-			   		$.ajax({
-			    		url: sunriseurl,
-			    		dataType: "JSONP",
-			    		success: function(data) {
-			    			$("#sunrise").text(data.results.sunrise);
-			    			console.log(data);
-			    		} 	//end of success function
-						}); //end of api call
+
+	   				var currentDate = new Date(startDateMilli);
+	 					var sunriseurl = "http://api.sunrise-sunset.org/json?lat=" + results[0].geometry.location.lat() + "&lng=" + results[0].geometry.location.lng() + "&date=" + currentDate + "&callback=mycallback";
+			   		var response = $.ajax({
+											    		url: sunriseurl,
+											    		dataType: "JSONP",
+											    		success: function(data) {
+											    			return data.results;
+											    		} 	//end of success function
+														}); //end of api call
+			   		console.log(response)
+						startDateMilli += 86400000;
 			   	} //end of while loop
   			}
 			});
